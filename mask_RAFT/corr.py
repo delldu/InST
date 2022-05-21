84f81a5077bd92c56023150365e44609189825d5
delldu <18588220928@163.com>
2022-05-21 14:02:00 +0800

    Study

--- mask_RAFT/corr.py
+++ mask_RAFT/corr.py
@@ -1,14 +1,14 @@
 import torch
 import torch.nn.functional as F
-from utils.utils import bilinear_sampler, coords_grid
+from utils.utils import bilinear_sampler
 from torchvision.utils import save_image
-import torch.nn as nn
 
-try:
-    import alt_cuda_corr
-except:
-    # alt_cuda_corr is not compiled
-    pass
+# try:
+#     import alt_cuda_corr
+# except:
+#     # alt_cuda_corr is not compiled
+#     pass
+
 
 class CorrBlock:
     def __init__(self, fmap1, fmap2, num_levels=4, radius=4):
@@ -17,7 +17,7 @@ class CorrBlock:
         self.corr_pyramid = []
 
         # all pairs correlation
-        corr = CorrBlock.corr(fmap1, fmap2)   # [B,H,W,1,H,W]
+        corr = CorrBlock.corr(fmap1, fmap2)  # [B,H,W,1,H,W]
 
         # local and global correlation
         # corr1 = CorrBlock.corr(fmap1.clone(), fmap2.clone())   # [B,H,W,1,H,W]
@@ -26,13 +26,13 @@ class CorrBlock:
         # corr = torch.sum(corr,dim=1)
 
         batch, h1, w1, dim, h2, w2 = corr.shape
-        corr = corr.reshape(batch*h1*w1, dim, h2, w2)
+        corr = corr.reshape(batch * h1 * w1, dim, h2, w2)
 
-        self.corr_map = corr.reshape(batch,h1*w1,h2,w2).clone()
+        self.corr_map = corr.reshape(batch, h1 * w1, h2, w2).clone()
         # self.corr_map = corr.reshape(batch,h1,w1,h2*w2).permute(0,3,1,2).clone()
-        
+
         self.corr_pyramid.append(corr)
-        for i in range(self.num_levels-1):
+        for i in range(self.num_levels - 1):
             corr = F.avg_pool2d(corr, 2, stride=2)
             self.corr_pyramid.append(corr)
 
@@ -44,12 +44,12 @@ class CorrBlock:
         out_pyramid = []
         for i in range(self.num_levels):
             corr = self.corr_pyramid[i]
-            dx = torch.linspace(-r, r, 2*r+1)
-            dy = torch.linspace(-r, r, 2*r+1)
+            dx = torch.linspace(-r, r, 2 * r + 1)
+            dy = torch.linspace(-r, r, 2 * r + 1)
             delta = torch.stack(torch.meshgrid(dy, dx), axis=-1).to(coords.device)
 
-            centroid_lvl = coords.reshape(batch*h1*w1, 1, 1, 2) / 2**i
-            delta_lvl = delta.view(1, 2*r+1, 2*r+1, 2)
+            centroid_lvl = coords.reshape(batch * h1 * w1, 1, 1, 2) / 2 ** i
+            delta_lvl = delta.view(1, 2 * r + 1, 2 * r + 1, 2)
             coords_lvl = centroid_lvl + delta_lvl
 
             corr = bilinear_sampler(corr, coords_lvl)
@@ -63,12 +63,12 @@ class CorrBlock:
     @staticmethod
     def corr(fmap1, fmap2):
         batch, dim, ht, wd = fmap1.shape
-        fmap1 = fmap1.view(batch, dim, ht*wd)
-        fmap2 = fmap2.view(batch, dim, ht*wd) 
-        
-        corr = torch.matmul(fmap1.transpose(1,2), fmap2)
+        fmap1 = fmap1.view(batch, dim, ht * wd)
+        fmap2 = fmap2.view(batch, dim, ht * wd)
+
+        corr = torch.matmul(fmap1.transpose(1, 2), fmap2)
         corr = corr.view(batch, ht, wd, 1, ht, wd)
-        return corr  / torch.sqrt(torch.tensor(dim).float())
+        return corr / torch.sqrt(torch.tensor(dim).float())
 
     # def corr2(self, fmap1, fmap2,q,k):
     #     num_heads = 2
@@ -85,32 +85,32 @@ class CorrBlock:
     #     return corr / torch.sqrt(torch.tensor(head_dim).float())
 
 
-class AlternateCorrBlock:
-    def __init__(self, fmap1, fmap2, num_levels=4, radius=4):
-        self.num_levels = num_levels
-        self.radius = radius
+# class AlternateCorrBlock:
+#     def __init__(self, fmap1, fmap2, num_levels=4, radius=4):
+#         self.num_levels = num_levels
+#         self.radius = radius
 
-        self.pyramid = [(fmap1, fmap2)]
-        for i in range(self.num_levels):
-            fmap1 = F.avg_pool2d(fmap1, 2, stride=2)
-            fmap2 = F.avg_pool2d(fmap2, 2, stride=2)
-            self.pyramid.append((fmap1, fmap2))
+#         self.pyramid = [(fmap1, fmap2)]
+#         for i in range(self.num_levels):
+#             fmap1 = F.avg_pool2d(fmap1, 2, stride=2)
+#             fmap2 = F.avg_pool2d(fmap2, 2, stride=2)
+#             self.pyramid.append((fmap1, fmap2))
 
-    def __call__(self, coords):
-        coords = coords.permute(0, 2, 3, 1)
-        B, H, W, _ = coords.shape
-        dim = self.pyramid[0][0].shape[1]
+#     def __call__(self, coords):
+#         coords = coords.permute(0, 2, 3, 1)
+#         B, H, W, _ = coords.shape
+#         dim = self.pyramid[0][0].shape[1]
 
-        corr_list = []
-        for i in range(self.num_levels):
-            r = self.radius
-            fmap1_i = self.pyramid[0][0].permute(0, 2, 3, 1).contiguous()
-            fmap2_i = self.pyramid[i][1].permute(0, 2, 3, 1).contiguous()
+#         corr_list = []
+#         for i in range(self.num_levels):
+#             r = self.radius
+#             fmap1_i = self.pyramid[0][0].permute(0, 2, 3, 1).contiguous()
+#             fmap2_i = self.pyramid[i][1].permute(0, 2, 3, 1).contiguous()
 
-            coords_i = (coords / 2**i).reshape(B, 1, H, W, 2).contiguous()
-            corr, = alt_cuda_corr.forward(fmap1_i, fmap2_i, coords_i, r)
-            corr_list.append(corr.squeeze(1))
+#             coords_i = (coords / 2 ** i).reshape(B, 1, H, W, 2).contiguous()
+#             (corr,) = alt_cuda_corr.forward(fmap1_i, fmap2_i, coords_i, r)
+#             corr_list.append(corr.squeeze(1))
 
-        corr = torch.stack(corr_list, dim=1)
-        corr = corr.reshape(B, -1, H, W)
-        return corr / torch.sqrt(torch.tensor(dim).float())
+#         corr = torch.stack(corr_list, dim=1)
+#         corr = corr.reshape(B, -1, H, W)
+#         return corr / torch.sqrt(torch.tensor(dim).float())
--- test.sh
+++ test.sh
@@ -1,10 +1,11 @@
-# python test_LGW.py \
-# 	--source_path images/running-images-mask/source_rgb \
-# 	--target_path images/running-images-mask/target/ \
-# 	--checkpoint pretrained/checkpoint_r3.pth
-
 python test_LGW.py \
 	--source_path images/running-images-mask/source_rgb/juice.png \
 	--source_mask_path images/running-images-mask/source/juice.png \
 	--target_mask_path images/running-images-mask/target/vase5_mask.jpg \
 	--checkpoint pretrained/checkpoint_r3.pth
+
+# python test_LGW.py \
+# 	--source_dir images/running-images-mask/source_rgb \
+# 	--source_mask_dir images/running-images-mask/source \
+# 	--target_mask_dir images/running-images-mask/target \
+# 	--checkpoint pretrained/checkpoint_r3.pth
